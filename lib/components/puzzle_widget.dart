c4fee993f951d7c6dad2322cc29efd149c66fe0d
Tal Oz Cohen <talo58948@gmail.com>
2021-03-16 15:32:43 +0200

    updated models

--- lib/components/puzzle_widget.dart
+++ lib/components/puzzle_widget.dart
@@ -1,11 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:web1/models/puzzle.dart';
 
 class PuzzleWidget extends StatelessWidget {
   final bool isHovered;
+  final Puzzle puzzle;
   // final
-  PuzzleWidget({this.isHovered = false});
+  PuzzleWidget({this.isHovered = false, this.puzzle}){
+    if(this.puzzle == null){
+      this.puzzle =Puzzle.Default();
+    }
+  };
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: [
+        Row(
+          children: [],
+        )
+      ],
+    );
   }
 }
