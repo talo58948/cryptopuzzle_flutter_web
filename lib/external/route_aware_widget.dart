09a9a3f69e2c2b44b8147526569b88d86010571f
Tal Oz Cohen <talo58948@gmail.com>
2021-03-23 00:37:52 +0200

    STABLE 0:37 23-03-2021

---
+++ lib/external/route_aware_widget.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+import '../manager.dart';
+
+final RouteObserver<PageRoute> routeObserver = RouteObserver<PageRoute>();
+
+class RouteAwareWidget extends StatefulWidget {
+  final Widget child;
+  final Pages page;
+
+  RouteAwareWidget({@required this.child, @required this.page});
+
+  @override
+  State<RouteAwareWidget> createState() => RouteAwareWidgetState();
+}
+
+// Implement RouteAware in a widget's state and subscribe it to the RouteObserver.
+class RouteAwareWidgetState extends State<RouteAwareWidget> with RouteAware {
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    routeObserver.subscribe(this, ModalRoute.of(context));
+  }
+
+  @override
+  void dispose() {
+    routeObserver.unsubscribe(this);
+    super.dispose();
+  }
+
+  @override
+  // Called when the current route has been pushed.
+  void didPush() {
+    print('didPush ${widget.page.toString()}');
+    Manager.onNavPush(widget.page);
+  }
+
+  @override
+  // Called when the top route has been popped off, and the current route shows up.
+  void didPopNext() {
+    print('didPopNext ${widget.page.toString()}');
+    Manager.onNavPop();
+  }
+
+  @override
+  Widget build(BuildContext context) => widget.child;
+}
