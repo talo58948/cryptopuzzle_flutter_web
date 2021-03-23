e84b23beef6606a59c2adbede64c6ac919f5366c
Tal Oz Cohen <talo58948@gmail.com>
2021-03-23 22:49:42 +0200

    added html meta data

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: """PuzzleTokens - the world's first NFT puzzles""",
       theme: ThemeData.dark(),
       navigatorObservers: [routeObserver],
       debugShowCheckedModeBanner: false,
